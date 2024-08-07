628ecedf546738f1851535480dc6bbfcd47b1ae1
Alan Fernandez <alan.f@msn.com>
2024-08-07 16:38:41 +0200

    eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+      },
+    ],
+  },
 };
